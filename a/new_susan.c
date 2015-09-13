bee658c7620a5ca885f6d20c3643bf913b6dfd89
Shuang Song <songshuang1990@gmail.com>
2015-09-13 12:57:56 -0500

    a code

---
+++ a/new_susan.c
@@ -0,0 +1,393 @@
+#include "new_susan.h"
+
+
+void main(int argc, char *argv[]){
+/*******vars*******************/
+unsigned char in[image_size];
+unsigned char bp[516];
+unsigned char mid[image_size];
+int r[image_size];
+/******************************/
+
+
+/*******functions calls*******************/
+
+get_image(argv[1],in);
+
+setup_brightness_lut(bp,20,6);
+
+//susan_edges(in,r,mid,bp,2650);
+
+//susan_thin(r,mid);
+
+edge_draw(in,mid,0);
+
+put_image(argv[2],in);
+
+/*****************************************/
+
+}
+
+
+
+/* }}} */
+/* {{{ put_image(filename,in,x_size,y_size) */
+
+put_image(char filename[100],uchar in[image_size]){
+FILE  *fd;
+
+#ifdef FOPENB
+  if ((fd=fopen(filename,"wb")) == NULL)
+#else
+  if ((fd=fopen(filename,"w")) == NULL)
+#endif
+    exit_error("Can't output image%s.\n",filename);
+
+  fprintf(fd,"P5\n");
+  fprintf(fd,"%d %d\n",x_size,y_size);
+  fprintf(fd,"255\n");
+
+  if (fwrite(in,x_size*y_size,1,fd) != 1)
+    exit_error("Can't write image %s.\n",filename);
+
+  fclose(fd);
+}
+
+
+
+/* }}} */
+/* {{{ edges */
+
+/* {{{ edge_draw(in,corner_list,drawing_mode) */
+
+void edge_draw(uchar in[image_size], uchar mid[image_size], int drawing_mode){
+int   i;
+uchar *inp, *midp;
+
+  if (drawing_mode==0)
+  {
+    /* mark 3x3 white block around each edge point */
+    midp=mid;
+    for (i=0; i<x_size*y_size; i++)
+    {
+      if (*midp<8)
+      {
+        inp = in + (midp - mid) - x_size - 1;
+        *inp++=255; *inp++=255; *inp=255; inp+=x_size-2;
+        *inp++=255; *inp++;     *inp=255; inp+=x_size-2;
+        *inp++=255; *inp++=255; *inp=255;
+      }
+      midp++;
+    }
+  }
+
+  /* now mark 1 black pixel at each edge point */
+  midp=mid;
+  for (i=0; i<x_size*y_size; i++)
+  {
+    if (*midp<8)
+      *(in + (midp - mid)) = 0;
+    midp++;
+  }
+}
+
+
+
+
+
+/* }}} */
+
+void get_image(char filename[200], unsigned char *in){
+
+FILE  *fd;
+char header [100];
+int  tmp;
+
+#ifdef FOPENB
+  if ((fd=fopen(filename,"rb")) == NULL)
+#else
+  if ((fd=fopen(filename,"r")) == NULL)
+#endif
+    exit_error("Can't input image %s.\n",filename);
+
+
+  /* {{{ read header */
+
+  header[0]=fgetc(fd);
+  header[1]=fgetc(fd);
+
+  if(!(header[0]=='P' && header[1]=='5'))
+    exit_error("Image %s does not have binary PGM header.\n",filename);
+
+//  *x_size = getint(fd);
+//  *y_size = getint(fd);
+//  tmp = getint(fd);
+
+/* }}} */
+
+//  *in = (uchar *) malloc(*x_size * *y_size);
+
+  if (fread(in,1,x_size * y_size,fd) == 0)
+    exit_error("Image %s is wrong size.\n",filename);
+
+  fclose(fd);
+}
+
+
+void setup_brightness_lut(unsigned char bp[516],int thresh, int form){
+
+int   k;
+float temp;
+
+  printf("thresh is %d\n",thresh);
+
+//  *bp=(unsigned char *)malloc(516);
+//  *bp=*bp+258;
+
+  bp = bp + 258;
+
+  for(k=-256;k<257;k++)
+  {
+    temp=((float)k)/((float)thresh);
+    temp=temp*temp;
+    if (form==6)
+      temp=temp*temp*temp;
+    temp=100.0*exp(-temp);
+    *(bp+k)= (uchar)temp;
+  }
+
+}
+
+/* }}} */
+/* {{{ susan_edges(in,r,sf,max_no,out) */
+
+/*
+void susan_edges(unsigned char in[image_size], int r[image_size],unsigned char mid[image_size],unsigned char bp[image_size], int max_no){
+
+float z;
+int   do_symmetry, i, j, m, n, a, b, x, y, w;
+uchar c,*p,*cp;
+
+  memset (r,0,x_size * y_size * sizeof(int));
+
+  for (i=3;i<y_size-3;i++)
+    for (j=3;j<x_size-3;j++)
+    {
+      n=100;
+      p=in + (i-3)*x_size + j - 1;
+      cp=bp + in[i*x_size+j];
+
+      n+=*(cp-*p++);
+      n+=*(cp-*p++);
+      n+=*(cp-*p);
+      p+=x_size-3; 
+
+      n+=*(cp-*p++);
+      n+=*(cp-*p++);
+      n+=*(cp-*p++);
+      n+=*(cp-*p++);
+      n+=*(cp-*p);
+      p+=x_size-5;
+
+      n+=*(cp-*p++);
+      n+=*(cp-*p++);
+      n+=*(cp-*p++);
+      n+=*(cp-*p++);
+      n+=*(cp-*p++);
+      n+=*(cp-*p++);
+      n+=*(cp-*p);
+      p+=x_size-6;
+
+      n+=*(cp-*p++);
+      n+=*(cp-*p++);
+      n+=*(cp-*p);
+      p+=2;
+      n+=*(cp-*p++);
+      n+=*(cp-*p++);
+      n+=*(cp-*p);
+      p+=x_size-6;
+
+      n+=*(cp-*p++);
+      n+=*(cp-*p++);
+      n+=*(cp-*p++);
+      n+=*(cp-*p++);
+      n+=*(cp-*p++);
+      n+=*(cp-*p++);
+      n+=*(cp-*p);
+      p+=x_size-5;
+
+      n+=*(cp-*p++);
+      n+=*(cp-*p++);
+      n+=*(cp-*p++);
+      n+=*(cp-*p++);
+      n+=*(cp-*p);
+      p+=x_size-3;
+
+      n+=*(cp-*p++);
+      n+=*(cp-*p++);
+      n+=*(cp-*p);
+
+      if (n<=max_no)
+        r[i*x_size+j] = max_no - n;
+    }
+
+  for (i=4;i<y_size-4;i++)
+    for (j=4;j<x_size-4;j++)
+    {
+      if (r[i*x_size+j]>0)
+      {
+        m=r[i*x_size+j];
+        n=max_no - m;
+        cp=bp + in[i*x_size+j];
+
+        if (n>600)
+        {
+          p=in + (i-3)*x_size + j - 1;
+          x=0;y=0;
+
+          c=*(cp-*p++);x-=c;y-=3*c;
+          c=*(cp-*p++);y-=3*c;
+          c=*(cp-*p);x+=c;y-=3*c;
+          p+=x_size-3;
+
+          c=*(cp-*p++);x-=2*c;y-=2*c;
+          c=*(cp-*p++);x-=c;y-=2*c;
+          c=*(cp-*p++);y-=2*c;
+          c=*(cp-*p++);x+=c;y-=2*c;
+          c=*(cp-*p);x+=2*c;y-=2*c;
+          p+=x_size-5;
+
+          c=*(cp-*p++);x-=3*c;y-=c;
+          c=*(cp-*p++);x-=2*c;y-=c;
+          c=*(cp-*p++);x-=c;y-=c;
+          c=*(cp-*p++);y-=c;
+          c=*(cp-*p++);x+=c;y-=c;
+          c=*(cp-*p++);x+=2*c;y-=c;
+          c=*(cp-*p);x+=3*c;y-=c;
+          p+=x_size-6;
+
+          c=*(cp-*p++);x-=3*c;
+          c=*(cp-*p++);x-=2*c;
+          c=*(cp-*p);x-=c;
+          p+=2;
+          c=*(cp-*p++);x+=c;
+          c=*(cp-*p++);x+=2*c;
+          c=*(cp-*p);x+=3*c;
+          p+=x_size-6;
+
+          c=*(cp-*p++);x-=3*c;y+=c;
+          c=*(cp-*p++);x-=2*c;y+=c;
+          c=*(cp-*p++);x-=c;y+=c;
+          c=*(cp-*p++);y+=c;
+          c=*(cp-*p++);x+=c;y+=c;
+          c=*(cp-*p++);x+=2*c;y+=c;
+          c=*(cp-*p);x+=3*c;y+=c;
+          p+=x_size-5;
+
+          c=*(cp-*p++);x-=2*c;y+=2*c;
+          c=*(cp-*p++);x-=c;y+=2*c;
+          c=*(cp-*p++);y+=2*c;
+          c=*(cp-*p++);x+=c;y+=2*c;
+          c=*(cp-*p);x+=2*c;y+=2*c;
+          p+=x_size-3;
+
+          c=*(cp-*p++);x-=c;y+=3*c;
+          c=*(cp-*p++);y+=3*c;
+          c=*(cp-*p);x+=c;y+=3*c;
+
+          z = sqrt((float)((x*x) + (y*y)));
+          if (z > (0.9*(float)n)) 
+          {
+            do_symmetry=0;
+            if (x==0)
+              z=1000000.0;
+            else
+              z=((float)y) / ((float)x);
+            if (z < 0) { z=-z; w=-1; }
+            else w=1;
+            if (z < 0.5) {  a=0; b=1; }
+            else { if (z > 2.0) {  a=1; b=0; }
+            else {  if (w>0) { a=1; b=1; }
+                                   else { a=-1; b=1; }}}
+            if ( (m > r[(i+a)*x_size+j+b]) && (m >= r[(i-a)*x_size+j-b]) &&
+                 (m > r[(i+(2*a))*x_size+j+(2*b)]) && (m >= r[(i-(2*a))*x_size+j-(2*b)]) )
+              mid[i*x_size+j] = 1;
+          }
+          else
+            do_symmetry=1;
+        }
+        else
+          do_symmetry=1;
+
+        if (do_symmetry==1)
+        {
+          p=in + (i-3)*x_size + j - 1;
+          x=0; y=0; w=0;
+
+
+          c=*(cp-*p++);x+=c;y+=9*c;w+=3*c;
+          c=*(cp-*p++);y+=9*c;
+          c=*(cp-*p);x+=c;y+=9*c;w-=3*c;
+          p+=x_size-3;
+
+          c=*(cp-*p++);x+=4*c;y+=4*c;w+=4*c;
+          c=*(cp-*p++);x+=c;y+=4*c;w+=2*c;
+          c=*(cp-*p++);y+=4*c;
+          c=*(cp-*p++);x+=c;y+=4*c;w-=2*c;
+          c=*(cp-*p);x+=4*c;y+=4*c;w-=4*c;
+          p+=x_size-5;
+
+          c=*(cp-*p++);x+=9*c;y+=c;w+=3*c;
+          c=*(cp-*p++);x+=4*c;y+=c;w+=2*c;
+          c=*(cp-*p++);x+=c;y+=c;w+=c;
+          c=*(cp-*p++);y+=c;
+          c=*(cp-*p++);x+=c;y+=c;w-=c;
+          c=*(cp-*p++);x+=4*c;y+=c;w-=2*c;
+          c=*(cp-*p);x+=9*c;y+=c;w-=3*c;
+          p+=x_size-6;
+
+          c=*(cp-*p++);x+=9*c;
+          c=*(cp-*p++);x+=4*c;
+          c=*(cp-*p);x+=c;
+          p+=2;
+          c=*(cp-*p++);x+=c;
+          c=*(cp-*p++);x+=4*c;
+          c=*(cp-*p);x+=9*c;
+          p+=x_size-6;
+
+          c=*(cp-*p++);x+=9*c;y+=c;w-=3*c;
+          c=*(cp-*p++);x+=4*c;y+=c;w-=2*c;
+          c=*(cp-*p++);x+=c;y+=c;w-=c;
+          c=*(cp-*p++);y+=c;
+          c=*(cp-*p++);x+=c;y+=c;w+=c;
+          c=*(cp-*p++);x+=4*c;y+=c;w+=2*c;
+          c=*(cp-*p);x+=9*c;y+=c;w+=3*c;
+          p+=x_size-5;
+
+          c=*(cp-*p++);x+=4*c;y+=4*c;w-=4*c;
+          c=*(cp-*p++);x+=c;y+=4*c;w-=2*c;
+          c=*(cp-*p++);y+=4*c;
+          c=*(cp-*p++);x+=c;y+=4*c;w+=2*c;
+          c=*(cp-*p);x+=4*c;y+=4*c;w+=4*c;
+          p+=x_size-3;
+
+          c=*(cp-*p++);x+=c;y+=9*c;w-=3*c;
+          c=*(cp-*p++);y+=9*c;
+          c=*(cp-*p);x+=c;y+=9*c;w+=3*c;
+
+          if (y==0)
+            z = 1000000.0;
+          else
+            z = ((float)x) / ((float)y);
+          if (z < 0.5) {  a=0; b=1; }
+          else { if (z > 2.0) {  a=1; b=0; }
+          else {  if (w>0) { a=-1; b=1; }
+                                else { a=1; b=1; }}}
+          if ( (m > r[(i+a)*x_size+j+b]) && (m >= r[(i-a)*x_size+j-b]) &&
+               (m > r[(i+(2*a))*x_size+j+(2*b)]) && (m >= r[(i-(2*a))*x_size+j-(2*b)]) )
+            mid[i*x_size+j] = 2;
+        }
+      }
+    }
+}
+*/
